cb9aa1abff989f404eb4b850522b0ef8305fa0d2
ali-rafey <alirafey14789@gmail.com>
2024-03-12 18:05:10 +0500

    Exhibition from user side is done

--- lib/User/throughNavBar/userExhibition/detailExhibationPage.dart
+++ lib/User/throughNavBar/userExhibition/detailExhibationPage.dart
@@ -1,79 +1,100 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-class DetailExhibitionPage extends StatelessWidget{
+class DetailExhibitionPage extends StatelessWidget {
+  final DocumentSnapshot exhibition;
+
+  DetailExhibitionPage({required this.exhibition});
+
   @override
-
   Widget build(BuildContext context) {
-    double screenHeight=MediaQuery.of(context).size.height;
-   return Scaffold(
-     body: Stack(
-       children: [
-         Container(
-           width: double.infinity,
-           height: 880,
-           child: Image.asset("assests/images/dbpic2.jpeg"
-             ,fit: BoxFit.fitHeight,),
-           //add background image here
-         ),
-         Container(
-           child: Column(
-             children: [
-               SizedBox(height: screenHeight*0.1,),
-               Center(
-                 child: Container(
-                   width: 300,
-                   height: 300,
-                   child: Image.asset("assests/images/userExhibitionDemo.jpg"),
-                 ),
-               ),
-               SizedBox(
-                 height:screenHeight*0.04,
-               ),
-               Text("Venue:",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
-               Text("19-A, Lailpull Galleria,Canal Road",style: TextStyle(fontSize: 20,fontWeight: FontWeight.w400),),
+    double screenHeight = MediaQuery.of(context).size.height;
+    String venue = exhibition['venue'];
+    String date = exhibition['date'];
+    String time = exhibition['time'];
 
-               SizedBox(
-                 height:screenHeight*0.02,
-               ),
-               Text("Date:",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
-               Text("December 21,2023",style: TextStyle(fontSize: 20,fontWeight: FontWeight.w400),),
-               SizedBox(
-                 height:screenHeight*0.02,
-               ),
-               Text("Time:",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
-               Text("10:15 PM",style: TextStyle(fontSize: 20,fontWeight: FontWeight.w400),),
-               SizedBox(
-                 height:screenHeight*0.04,
-               ),
-               Container(
-                 width: 200,
-                 height: 50,
-                 child: ElevatedButton(onPressed:()  {
-                   //add Maps here
-                 }, child:Row(
-                   children: [
-                     FaIcon(FontAwesomeIcons.mapLocationDot,color: Colors.black,),
-                     SizedBox(width: 25,),
-                     Text("Location",style: TextStyle(color: Colors.white,fontWeight: FontWeight.w800,fontSize: 20),),
-                   ],
-                 ),
-
-                   style: ElevatedButton.styleFrom(
-                       backgroundColor: Colors.green,
-                       shape: RoundedRectangleBorder(
-                           borderRadius: BorderRadius.circular(15.0)
-                       )
-                   ),
-                 ),
-               )
-             ],
-           ),
-         ),
-       ],
-     ),
-   );
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            width: double.infinity,
+            height: 880,
+            child: Image.asset(
+              "assests/images/dbpic2.jpeg",
+              fit: BoxFit.fitHeight,
+            ),
+          ),
+          Container(
+            child: Column(
+              children: [
+                SizedBox(height: screenHeight * 0.1),
+                Center(
+                  child: Container(
+                    width: 300,
+                    height: 300,
+                    child: Image.asset("assests/images/userExhibitionDemo.jpg"),
+                  ),
+                ),
+                SizedBox(height: screenHeight * 0.04),
+                Text(
+                  "Venue:",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  venue,
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
+                ),
+                SizedBox(height: screenHeight * 0.02),
+                Text(
+                  "Date:",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  date,
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
+                ),
+                SizedBox(height: screenHeight * 0.02),
+                Text(
+                  "Time:",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  time,
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
+                ),
+                SizedBox(height: screenHeight * 0.04),
+                Container(
+                  width: 200,
+                  height: 50,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      // Add Maps here
+                    },
+                    child: Row(
+                      children: [
+                        FaIcon(FontAwesomeIcons.mapLocationDot, color: Colors.black),
+                        SizedBox(width: 25),
+                        Text(
+                          "Location",
+                          style: TextStyle(color: Colors.white, fontWeight: FontWeight.w800, fontSize: 20),
+                        ),
+                      ],
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.green,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15.0),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
-  
 }
--- lib/User/throughNavBar/userExhibition/userExhibitionPage.dart
+++ lib/User/throughNavBar/userExhibition/userExhibitionPage.dart
@@ -1,79 +1,95 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
-
 import '../../Navbar/userNavbar.dart';
 import 'detailExhibationPage.dart';
 
-class UserExhibitionPage extends StatelessWidget{
+class UserExhibitionPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-   return Scaffold(
-     drawer: UserNavbar(),
-     appBar: AppBar(
-       title: const Text("Exhibitions", style: TextStyle(color: Colors.white, fontWeight: FontWeight.w600, fontSize: 30)),
-       centerTitle: true,
-       backgroundColor: Colors.transparent,
-       elevation: 0,
-       flexibleSpace: Container(
-         decoration: BoxDecoration(
-           gradient: LinearGradient(
-             begin: Alignment.centerLeft,
-             end: Alignment.centerRight,
-             colors: [
-               Color(0xffF0A2C9),
-               Color(0xffD2A5D0),
-               Color(0xff6F9BB4),
+    return Scaffold(
+      drawer: UserNavbar(),
+      appBar: AppBar(
+        title: const Text("Exhibitions", style: TextStyle(color: Colors.white, fontWeight: FontWeight.w600, fontSize: 30)),
+        centerTitle: true,
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.centerLeft,
+              end: Alignment.centerRight,
+              colors: [
+                Color(0xffF0A2C9),
+                Color(0xffD2A5D0),
+                Color(0xff6F9BB4),
+              ],
+            ),
+          ),
+        ),
+      ),
+      body: Stack(
+        children: [
+          Container(
+            width: double.infinity,
+            height: 880,
+            child: Image.asset(
+              "assests/images/dbpic2.jpeg",
+              fit: BoxFit.fitHeight,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: StreamBuilder<QuerySnapshot>(
+              stream: FirebaseFirestore.instance.collection('exhibition').snapshots(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+                if (snapshot.hasError) {
+                  return Center(
+                    child: Text('Error: ${snapshot.error}'),
+                  );
+                }
+                final exhibitions = snapshot.data!.docs;
 
-             ],
-           ),
-         ),
-       ),     ),
-     body:Stack(
-       children: [
-         Container(
-           width: double.infinity,
-           height: 880,
-           child: Image.asset("assests/images/dbpic2.jpeg"
-             ,fit: BoxFit.fitHeight,),
-           //add background image here
-         ),
-         Padding(
-           padding: const EdgeInsets.all(16.0),
-           child: Column(
-             children: [
-               Expanded(
-                 child: ListView.separated(
-                   itemCount: 5, // Number of exhibitions (fetch from the database)
-                   itemBuilder: (context, index) {
-                     return Slidable(
-                       actionPane: SlidableDrawerActionPane(),
-                       actionExtentRatio: 0.25,
-                       child: Container(
-                         decoration: BoxDecoration(
-                           color: Colors.white,
-                             borderRadius: BorderRadius.circular(15.0)
-                         ),
-                         child: ListTile(
-                           leading: ClipOval(child: Image.asset("assests/images/userExhibitionDemo.jpg")),
-                           title: Text('Exhibition ${index + 1}'),
-                           subtitle: Text('Venue, Date, Time'),
-                           onTap: () {
-                             Navigator.push(context, MaterialPageRoute(builder: (context)=>DetailExhibitionPage()));
-                           },
-                         ),
-                       ),
-                     );
-                   }, separatorBuilder: (BuildContext context, int index) { return const Divider(); },
-                 ),
-               ),
-
-             ],
-           ),
-         ) ,
-       ],
-     )
-   );
+                return ListView.separated(
+                  itemCount: exhibitions.length,
+                  itemBuilder: (context, index) {
+                    DocumentSnapshot doc = exhibitions[index];
+                    return Slidable(
+                      actionPane: SlidableDrawerActionPane(),
+                      actionExtentRatio: 0.25,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(15.0),
+                        ),
+                        child: ListTile(
+                          leading: ClipOval(child: Image.asset("assests/images/userExhibitionDemo.jpg")),
+                          title: Text('Exhibition ${index + 1}'),
+                          subtitle: Text('Venue, Date, Time'),
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => DetailExhibitionPage(exhibition: doc)),
+                            );
+                          },
+                        ),
+                      ),
+                    );
+                  },
+                  separatorBuilder: (BuildContext context, int index) {
+                    return const Divider();
+                  },
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
   }
-
 }
